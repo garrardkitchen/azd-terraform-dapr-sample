6943a2a3a1c9d1b6e468e2109ba96d1f393cd3de
Garrard Kitchen <garrardkitchen@gmail.com>
2025-12-06 21:58:35 +0000

    refactor: Refactored AppHost for improved Dapr and Azure Service Bus integration and added issues to README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dapr-test
 
-Infrastructure and Aspire app sample using Azure Developer CLI (`azd`).
+Terraform Aspire sample, inner loop development and deploy using Azure Developer CLI (`azd`).
 
 ## Infrastructure
 
@@ -27,4 +27,13 @@ azd up -e dev
 
 **Step 2**:
 
-You must now enable dapr for both apps.  Use the app name for the dapr id and port 8080 for both.
+You must now enable dapr for both apps (see **Issue 1** before).  Use the app name for the dapr id and port 8080 for both.
+
+---
+
+## Issues
+
+| # | Issue                                                                                                                                                                              | Work around                                                                                              | Viable | Why                                                                                                                                           |
+|---|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|----------------------------------------------------------------------------------------------------------|---|-----------------------------------------------------------------------------------------------------------------------------------------------|
+| 1 | The dapr sidecar configuration for both the apiservice and frontend are correctly configured by Terraform. However, when an application is deployed, it removes this configuration | Do not use `azd publish`. Instead, build, push and update image using az cli instead                                                                                              | ❌ | temporary loss of functionality between applications. Will fail to publish and consume messages, state will not be persist and be accessible. |
+| 2 | Setting the image name in Terraform before it has been built, will make the `terraform apply` fail.                                                                                | Use a standard public image, `lifecycle {ignore_changes}` for the image, then let the azd publish set it                                                                           | ✅ | -                                                                                                                                             |
--- dapr-test.AppHost/AppHost.cs
+++ dapr-test.AppHost/AppHost.cs
@@ -1,99 +1,64 @@
-using Aspire.Hosting.Azure;
 using CommunityToolkit.Aspire.Hosting.Dapr;
-using Azure.Provisioning;
 
-var builder = DistributedApplication.CreateBuilder(args);
-
-var acaEnv = builder
-    .AddAzureContainerAppEnvironment("dev")
-    .WithDaprSidecar()
-    .WithDashboard();
-
+IResourceBuilder<IDaprComponentResource> pubsub;
 string pubsubName = "pubsub-servicebus";
-
-var serviceBus = builder.AddAzureServiceBus("sbemulatorns")
-    .OnResourceReady(async (resource, evt, cancellationToken) =>
-{
-    var conn = await resource.ConnectionStringExpression.GetValueAsync(cancellationToken);
-    var mi = builder.Configuration["principalId"];
-    var ns = $"{conn.Split(";")[0].Split("sb://")[1]}.servicebus.windows.net";
-    var yaml = string.Empty;
-
-    if (builder.ExecutionContext.IsRunMode)  {
-      yaml = $"""
-              apiVersion: dapr.io/v1alpha1
-              kind: Component
-              metadata:
-                name: {pubsubName}
-              spec:
-                type: pubsub.azure.servicebus.topics
-                version: v1
-                metadata:
-                  - name: connectionString
-                    value: {conn}
-                  - name: disableEntityManagement
-                    value: "true"
-                  - name: consumerID
-                    value: "apiservice-subscription"    
-            """;
-    } 
-    else
-    {
-      // we don't need this as it's defined in the main.tf
-      yaml = $"""
-              apiVersion: dapr.io/v1alpha1
-              kind: Component
-              metadata:
-                name: {pubsubName}
-              spec:
-                type: pubsub.azure.servicebus.topics
-                version: v1
-                metadata:
-                  - name: namespaceName
-                    value: {ns}
-                  - name: azureClientId
-                    value: {mi}
-                  - name: consumerID
-                    value: "apiservice-subscription"    
-            """;
-    }
-    var filePath = Path.Combine(".",".dapr","components","pubsub.yaml");
-    Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
-    await File.WriteAllTextAsync(filePath, yaml, cancellationToken);
-});
-
-if (builder.ExecutionContext.IsRunMode)
-{
-    serviceBus
-        .RunAsEmulator(_ => { _.WithLifetime(ContainerLifetime.Persistent); });
-}
-
+var builder = DistributedApplication.CreateBuilder(args);
+builder.AddAzureContainerAppEnvironment("dev").WithDaprSidecar().WithDashboard();
+var serviceBus = builder.AddAzureServiceBus("sbemulatorns");
 var topic = serviceBus.AddServiceBusTopic("topic");
 
 if (builder.ExecutionContext.IsRunMode) {
+    
+    serviceBus
+        .RunAsEmulator(_ => { _.WithLifetime(ContainerLifetime.Persistent); });
 
-  topic.AddServiceBusSubscription("apiservice-subscription")
-    .WithProperties(subscription =>
+    serviceBus.OnResourceReady(async (resource, evt, cancellationToken) =>
     {
-        subscription.MaxDeliveryCount = 10;
+        var conn = await resource.ConnectionStringExpression.GetValueAsync(cancellationToken);
+        var mi = builder.Configuration["principalId"];
+        var ns = $"{conn.Split(";")[0].Split("sb://")[1]}.servicebus.windows.net";
+        var yaml = $"""
+                  apiVersion: dapr.io/v1alpha1
+                  kind: Component
+                  metadata:
+                    name: {pubsubName}
+                  spec:
+                    type: pubsub.azure.servicebus.topics
+                    version: v1
+                    metadata:
+                      - name: connectionString
+                        value: {conn}
+                      - name: disableEntityManagement
+                        value: "true"
+                      - name: consumerID
+                        value: "apiservice-subscription"    
+                """;
+        var filePath = Path.Combine(".", ".dapr", "components", "pubsub.yaml");
+        Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
+        await File.WriteAllTextAsync(filePath, yaml, cancellationToken);
+    });
+
+    topic.AddServiceBusSubscription("apiservice-subscription")
+        .WithProperties(subscription =>
+        {
+            subscription.MaxDeliveryCount = 10;
+        });
+  
+    pubsub = builder.AddDaprPubSub(pubsubName, new DaprComponentOptions
+    {
+      LocalPath = Path.Combine(".",".dapr","components","pubsub.yaml")
     });
 }
-
-var pubsub2 = builder.AddDaprPubSub(pubsubName, new DaprComponentOptions
+else
 {
-    LocalPath = Path.Combine(".",".dapr","components","pubsub.yaml")
-});
+    pubsub = builder.AddDaprPubSub(pubsubName);
+}
 
 var apiService = builder.AddProject<Projects.dapr_test_ApiService>("apiservice")
     .WithHttpHealthCheck("/health")
     .WaitFor(serviceBus)
     .WithReference(serviceBus)
     .WithReference(topic);
-
-if (builder.ExecutionContext.IsRunMode)
-{
-    apiService.WithDaprSidecar(sidecar => sidecar.WithReference(pubsub2));
-}
 
 var frontend = builder.AddProject<Projects.dapr_test_Web>("webfrontend")
     .WithExternalHttpEndpoints()
@@ -106,7 +71,8 @@ var frontend = builder.AddProject<Projects.dapr_test_Web>("webfrontend")
 
 if (builder.ExecutionContext.IsRunMode)
 {
-    frontend.WithDaprSidecar(sidecar => sidecar.WithReference(pubsub2));
+    frontend.WithDaprSidecar(sidecar => sidecar.WithReference(pubsub));
+    apiService.WithDaprSidecar(sidecar => sidecar.WithReference(pubsub));
 }
 
 builder.Build().Run();
